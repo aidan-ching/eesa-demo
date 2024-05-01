d51d225423ca35616e6b3ffb3fdccc9e278184a6
Aidan Ching <achin053@ucr.edu>
2024-05-01 14:51:03 -0700

    fix deployment

--- app/login/page.js
+++ app/login/page.js
@@ -1,27 +1,27 @@
-import { signIn } from "@/auth";
+// import { signIn } from "@/auth";
 
-export default function Login() {
-  return (
-    <div className="w-screen h-screen flex justify-center items-center">
-      <div className="w-[80%] h-[80%] md:w-[25%] md:h-[50%] md:border md:shadow md:rounded">
-        <form
-          className="flex flex-col p-5"
-          action={async (formData) => {
-            "use server";
-            await signIn("credentials", formData);
-          }}
-        >
-          <label>
-            Email:
-            <input name="email" type="email" />
-          </label>
-          <label>
-            Password:
-            <input name="password" type="password" />
-          </label>
-          <button>Sign In</button>
-        </form>
-      </div>
-    </div>
-  );
-}
+// export default function Login() {
+//   return (
+//     <div className="w-screen h-screen flex justify-center items-center">
+//       <div className="w-[80%] h-[80%] md:w-[25%] md:h-[50%] md:border md:shadow md:rounded">
+//         <form
+//           className="flex flex-col p-5"
+//           action={async (formData) => {
+//             "use server";
+//             await signIn("credentials", formData);
+//           }}
+//         >
+//           <label>
+//             Email:
+//             <input name="email" type="email" />
+//           </label>
+//           <label>
+//             Password:
+//             <input name="password" type="password" />
+//           </label>
+//           <button>Sign In</button>
+//         </form>
+//       </div>
+//     </div>
+//   );
+// }
