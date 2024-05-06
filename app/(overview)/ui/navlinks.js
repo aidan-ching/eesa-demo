c86fbbae9ceba75c55892c43f1367f0e117ef44c
Aidan Ching <achin053@ucr.edu>
2024-05-06 14:09:48 -0700

    Added icons to navigation bar

--- app/(overview)/ui/navlinks.js
+++ app/(overview)/ui/navlinks.js
@@ -1,4 +1,4 @@
-'use client'
+"use client";
 
 import { usePathname } from "next/navigation";
 
@@ -7,38 +7,53 @@ import { CiGrid41 } from "react-icons/ci";
 import { BiSquareRounded } from "react-icons/bi";
 import { MdOutlineTune } from "react-icons/md";
 
+import {
+  BookOpenIcon,
+  ExclamationCircleIcon,
+  BellAlertIcon,
+  ChartBarSquareIcon,
+} from "@heroicons/react/24/outline";
+
 export default function NavLinks() {
   const path = usePathname();
 
   return (
     <div className="w-full">
       <Link href="/">
-        <div className={`hover:bg-[#F3F3F4] px-3 py-2 mx-2 text-[#71717A] font-medium rounded-lg flex flex-row items-center ${path === '/' ? 'bg-[#F3F3F4]' : ''}`}>
+        <div
+          className={`hover:bg-[#F3F3F4] px-3 py-2 mx-2 text-[#71717A] font-medium rounded-lg flex flex-row items-center ${
+            path === "/" ? "bg-[#F3F3F4]" : ""
+          }`}
+        >
           <CiGrid41 size="20" color="#71717A" className="mr-2" />
           Overview
         </div>
       </Link>
       <Link href="/">
         <div className="hover:bg-[#F3F3F4] px-3 py-2 mx-2 text-[#71717A] font-medium rounded-lg flex flex-row items-center">
-          <BiSquareRounded size="15" color="#71717A" className="mr-3.5" />
+          <BookOpenIcon className="w-[18px] h-[18px] mr-3" />
           Boards
         </div>
       </Link>
       <Link href="/risk">
-        <div className={`hover:bg-[#F3F3F4] px-3 py-2 mx-2 text-[#71717A] font-medium rounded-lg flex flex-row items-center ${path === '/risk' ? 'bg-[#F3F3F4]' : ''}`}>
-          <BiSquareRounded size="15" color="#71717A" className="mr-3.5 ml-4" />
+        <div
+          className={`hover:bg-[#F3F3F4] px-3 py-2 mx-2 text-[#71717A] font-medium rounded-lg flex flex-row items-center ${
+            path === "/risk" ? "bg-[#F3F3F4]" : ""
+          }`}
+        >
+          <ExclamationCircleIcon className="w-[23px] h-[23px] mr-3 ml-4" />
           Risk
         </div>
       </Link>
       <Link href="/">
         <div className="hover:bg-[#F3F3F4] px-3 py-2 mx-2 text-[#71717A] font-medium rounded-lg flex flex-row items-center">
-          <BiSquareRounded size="15" color="#71717A" className="mr-3.5 ml-4" />
+          <ChartBarSquareIcon className="w-[23px] h-[23px] mr-3 ml-4" />
           Opportunities
         </div>
       </Link>
       <Link href="/">
         <div className="hover:bg-[#F3F3F4] px-3 py-2 mx-2 text-[#71717A] font-medium rounded-lg flex flex-row items-center">
-          <BiSquareRounded size="15" color="#71717A" className="mr-3.5 ml-4" />
+          <BellAlertIcon className="w-[23px] h-[23px] mr-3 ml-4" />
           Issues
         </div>
       </Link>
